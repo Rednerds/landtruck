5caa49c4ae108a8c73d5bb8c3b30b0e3b2df5e4a
Boris Gushin <boris@rocketguys.com>
2020-11-14 01:21:27 +0300

    fix

--- lib/shrine/plugins/refresh_remote_meta.rb
+++ lib/shrine/plugins/refresh_remote_meta.rb
@@ -3,6 +3,15 @@
 class Shrine
   module Plugins
     module RefreshRemoteMeta
+      module AttachmentMethods
+        def define_model_methods(name)
+          super if defined?(super)
+          define_method :"update_#{name}_metadata" do
+            send(:"#{name}_attacher").refresh_metadata!
+          end
+        end
+      end
+
       module AttacherMethods
         def refresh_metadata!(**options)
           file!.refresh_metadata!(**context, **options)
